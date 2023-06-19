09554ef6775b50ed409b2e0c889df116daf85009
kweenDev <refiloe15radebe@gmail.com>
2023-06-20 01:21:39 +0200

    Add a C program that prints a quote followed by a new line to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
+* main - prints a quote, followed by a new line, to the standard eror
 * Description: print a quote using write function
-*	write(int fd.const void *buf.size_t count)
-* Return: 1 (not success)
+* write(int fd.const void *buf.size_t count)
+* Return: 1 not success
 */
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1.quo.59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
