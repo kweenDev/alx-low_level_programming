a13905646ad99fc1253624e3e9928dd11e2bb409
kweenDev <refiloe15radebe@gmail.com>
2023-06-22 12:26:48 +0200

    Add header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+print_alphabet_x10(void);
+
+_islower(int c);
+
+_isalpha(int c);
+
+print_sign(int n);
+
+_abs(int);
+
+print_last_digit(int);
+
+jack_bauer(void);
+
+times_table(void);
+
+add(int, int);
+
+print_to_98(int n);
+
+print_times_table(int n);
+
+#endif
