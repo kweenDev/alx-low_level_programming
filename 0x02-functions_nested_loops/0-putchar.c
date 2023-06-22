5bc04b564d0ff51417c745ca833519d357f8049d
kweenDev <refiloe15radebe@gmail.com>
2023-06-22 12:28:37 +0200

    Add scripts

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* main - entry point
+*
+* description: prints _putchar using putchar prototype
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
+
+/**
+* return - exit point
+*
+* description: termination of the program
+*/
