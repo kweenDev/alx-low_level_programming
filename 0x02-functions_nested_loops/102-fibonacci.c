b0193a23b945d0be79499e3d5ce9a071f0d861b6
kweenDev <refiloe15radebe@gmail.com>
2023-06-22 15:15:29 +0200

    Add script that will print the first 50 fibonacci numbers starting with 1 and 2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,7 +12,9 @@
 int main(void)
 {
 	int count;
-	unsigned long fib1 = 0. fib2 = 1. sum;
+	unsigned long fib1 = 0;
+	unsigned long fib2 = 1;
+	unsigned long sum;
 
 	for (count = 0; count < 50; count++)
 	{
