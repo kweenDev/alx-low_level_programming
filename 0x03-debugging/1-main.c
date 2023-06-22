f1b2ec51eba3259f429a1cebe09a96b45736e532
kweenDev <refiloe15radebe@gmail.com>
2023-06-22 19:29:12 +0200

    Commit: Fix infinite loop in '1-main.c', commented out the code causing the infinite loop in the main function.

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,13 +5,12 @@
 * Return: 0
 */
 
-
 int main(void)
 {
-
 	int i;
 
 	printf("Infinite loop incoming:(\n");
+
 	i = 0;
 
 	/*while (i < 10)*/
@@ -20,5 +19,6 @@ int main(void)
 	/*}*/
 
 	printf("Infinite loop avoided!\\o/\n");
+
 	return (0);
 }
