1eb2a373a616b5f390172812398e59ac5957a30e
kweenDev <refiloe15radebe@gmail.com>
2023-07-04 03:38:06 +0200

    Implement memset function to fill memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,8 @@
 #include "main"
 
 /**
-* *_memset - fills memory with a constant byte
+* _memset - fills memory with a constant byte
+*
 * @s: memory area to be filled
 * @b: char to copy
 * @n: number of times to copy b
