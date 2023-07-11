c4f1201f125006039bb120777b53b6bb623e0e54
kweenDev <refiloe15radebe@gmail.com>
2023-07-11 18:22:19 +0200

    Fix variable declarations and unlock the power of words with strtow function

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,14 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 
+int wordCounterRec(char *str, int i);
+int word_counter(char *str);
+
 /**
-* wordCounterRec - Counts the number of words in a
-* string recursively.
-* @str: The string pointer
-* @i: The current index
-*
-* Return: Number of words in the string.
-*/
+ * wordCounterRec - Counts the number of words in a string recursively.
+ * @str: The string pointer.
+ * @i: The current index.
+ *
+ * Return: Number of words in the string.
+ */
 int wordCounterRec(char *str, int i)
 {
 	if (str[i] == '\0')
@@ -19,66 +21,73 @@ int wordCounterRec(char *str, int i)
 }
 
 /**
-* word_counter - Counts the number of words in 1-D
-* array of strings.
-* @str: The input string
-*
-* Return: Number of words in the string.
-*/
+ * word_counter - Counts the number of words in a 1-D array of strings.
+ * @str: The input string.
+ *
+ * Return: Number of words in the string.
+ */
 int word_counter(char *str)
 {
+	int words;
+
 	if (str[0] != ' ')
 		return (1 + wordCounterRec(str, 0));
 	return (wordCounterRec(str, 0));
 }
 
 /**
-* strtow - Splits a string into words.
-* @str: The input string
-*
-* Return: Pointer to an array of strings (words),
-* or NULL if str is NULL, or empty or if malloc fails.
-*/
+ * strtow - Splits a string into words.
+ * @str: The input string.
+ *
+ * Return: Pointer to an array of strings (words),
+ * or NULL if str is NULL, empty, or if malloc fails.
+ */
 char **strtow(char *str)
 {
 	char **strDup;
 	int i, n, m, words;
 
-	if (str == NULL || str[0] == 0)
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
+
 	words = word_counter(str);
 	if (words < 1)
 		return (NULL);
+
 	strDup = malloc(sizeof(char *) * (words + 1));
 	if (strDup == NULL)
 		return (NULL);
+
 	i = 0;
 	while (i < words && *str != '\0')
 	{
 		if (*str != ' ')
 		{
 			n = 0;
-			while (str[n] != ' ')
+			while (str[n] != ' ' && str[n] != '\0')
 				n++;
+
 			strDup[i] = malloc(sizeof(char) * (n + 1));
 			if (strDup[i] == NULL)
 			{
 				while (--i >= 0)
-					free(strDup[--i]);
+					free(strDup[i]);
 				free(strDup);
 				return (NULL);
 			}
+
 			m = 0;
 			while (m < n)
 			{
-				strDup[i][m] = *str;
-				m++, str++;
+				strDup[i][m] = str[m];
+				m++;
 			}
 			strDup[i][m] = '\0';
 			i++;
 		}
 		str++;
 	}
-	strDup[i] = '\0';
+	strDup[i] = NULL;
 	return (strDup);
 }
+
