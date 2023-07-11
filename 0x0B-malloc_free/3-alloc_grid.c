86efb4d62b3a36fbd8151c4d660f9422ff77541b
kweenDev <refiloe15radebe@gmail.com>
2023-07-11 10:30:46 +0200

    Fix C code syntax and shape dreams into reality wi th alloc_grid function

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,20 +22,20 @@ int **alloc_grid(int width, int height)
 	if (grid == NULL)
 		return (NULL);
 
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
 
 		if (grid[i] == NULL)
 		{
 			/* Free previously allocated memory */
-			for (int j = 0; j < i; j++)
+			for (j = 0; j < i; j++)
 				free(grid[j]);
 
 			free(grid);
 			return (NULL);
 		}
-		for (int j = 0; j < width; j++)
+		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}
 	return (grid);
