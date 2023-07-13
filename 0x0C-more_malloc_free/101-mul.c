85e92259f6800761b3873dd5d02c3cd254dfc201
kweenDev <refiloe15radebe@gmail.com>
2023-07-13 21:18:08 +0200

    Fix compilation errors and unveil the secrets of multiplication in the mystical realm of 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,115 +1,126 @@
-#include <stdlib.h>
-#include <unistd.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
-* _print_number - prints an integer to stdout
-* @n: The number to print
-*/
-void _print_number(int n)
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
+ *
+ * Return: no return.
+ */
+void _is_zero(char *argv[])
 {
-	if (n < 0)
-	{
-		_putchar(45);
-		n = -n;
-	}
+	int i, isn1 = 1, isn2 = 1;
 
-	if (n > 9)
-	{
-		_print_number(n / 10);
-	}
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
 
-	_putchar((n % 10) + '0');
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
 }
 
 /**
-* _is_digit - checks if a character is a digit
-* @c: The character to check
-*
-* Return: 1 if the character is a digit, 0 otherwise
-*/
-int _is_digit(char c)
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
 {
-	return (c >= '0' && c <= '9');
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
 }
 
 /**
-* _parse_number - parse a string and returns the
-* number it represents
-* @s: The string to parse
-*
-* Return: The number the string represents
-*/
-int _parse_number(char *s)
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
+int _checknum(char *argv[], int n)
 {
-	int n = 0, sign = 1;
+	int ln;
 
-	if (*s == '-')
-	{
-		sign = -1;
-		s++;
-	}
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
 
-	while (_is_digit(*s))
-	{
-		n = n * 10 + *s - '0';
-		s++;
-	}
-
-	return (n *sign);
+	return (ln);
 }
 
 /**
-* _mul - multiplies two positive integers
-* @a: The first operand
-* @b: The second operand
-*
-* Return: The product of a and b
-*/
-int _mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
-* main - multiplies two positive numbers
-* @argc: The number of arguments passed to the program
-* @argv: The array of arguments passed to the program
-*
-* Return: 0 on success, 98 on error
-*/
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
+ */
 int main(int argc, char *argv[])
 {
-	int num1, num2, res;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
 	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		exit(98);
+		if (i < 0)
+		{
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
 	}
-
-	num1 = _parse_number(argv[1]);
-	num2 = _parse_number(argv[2]);
-
-	if (num1 < 0 || num2 < 0)
-	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		exit(98);
-	}
-
-	res = _mul(num1, num2);
-	_print_number(res);
-	_putchar('\n');
-
+	printf("%s\n", nout);
 	return (0);
 }
