7ac7dc1cde6acb218a97d3fcb6cfa6eb880ef5c4
kweenDev <refiloe15radebe@gmail.com>
2023-07-21 20:11:22 +0200

    Fix conditional statement to output correctly

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("nil");
 
-		if (separator != NULL && i < n - 1)
+		if (separator != NULL && i < n - 1 && va_arg(args, const char *) != NULL)
 			printf("%s", separator);
 	}
 
