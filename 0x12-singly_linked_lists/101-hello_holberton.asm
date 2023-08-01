ac7820d4e64c048627a9be14da7d80649908200a
kweenDev <refiloe15radebe@gmail.com>
2023-08-01 18:40:06 +0200

    Unblocked all errors and unlocked the secrets of assembly magic using printf!

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,22 @@
 section .data
-    hello db 'Hello, Holberton', 0
+	hello db 'Hello, Holberton', 0
+	format db '%s', 10, 0	; Format string for printf, with a newline character (10)
 
 section .text
-    global _start
+	extern printf
 
-_start:
-    ; Print the message
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, hello
-    mov rdx, 16
-    syscall
+global main
 
-    ; Exit the program
-    mov rax, 60
-    xor edi, edi
-    syscall
+main:
+	; Set up the stack frame for printf
+	push rbp
+	mov rdi, format	; First argument: Format string
+	mov rsi, hello	; Second argument: Pointer to the string to be printed
+	xor rax, rax	; Clear rax to indicate no vector registers are used
+	call printf	; Call the printf function
+	pop rbp		; Restore the stack frame
+
+	; Exit the program
+	mov rax, 60	; System call number for sys_exit (60)
+	xor edi, edi	; Exit status 0
+	syscall
