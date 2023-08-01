589394189b8d8bb4c8eb8d2fc57089ae71128560
kweenDev <refiloe15radebe@gmail.com>
2023-08-02 01:27:05 +0200

    Freeing Nodes with Double The Magic

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* free_listint2 - Frees a listint_t list and sets the head to NULL.
+* @head: Pointer to a pointer to the head of the list.
+*/
+void free_listint2(listint_t **head)
+{
+	if (head)
+	{
+		while (*head)
+		{
+			listint_t *tmp = *head;
+			*head = (*head)->next;
+			free(tmp);
+		}
+	}
+}
