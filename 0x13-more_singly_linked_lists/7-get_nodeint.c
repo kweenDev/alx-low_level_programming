e41c16f0166d652136a810de0123fa00f36aae90
kweenDev <refiloe15radebe@gmail.com>
2023-08-02 01:28:41 +0200

    Finding Nodes with the Eye of a Data Sorcerer

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* get_nodeint - Returns the nth node of a listint_t list.
+* @head: Pointer to the head of the list.
+* @index: Index of the node to be returned (starting from 0).
+*
+* Return: The nth node or NULL if it doesn't exist.
+*/
+listint_t *get_nodeint(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
