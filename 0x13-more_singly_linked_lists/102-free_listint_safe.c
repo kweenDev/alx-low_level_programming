c2bee3fa53b2d4bedd620b7e4a8887c2e483f854
kweenDev <refiloe15radebe@gmail.com>
2023-08-02 01:32:47 +0200

    Freeing Lists with a Charm of Safety

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+* free_listint_safe - Frees a listint_t linked list.
+* @h: Pointer to a pointer to the head of the list.
+*
+* Return: The size of the list that was freed.
+*/
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *current, *temp;
+
+	if (!h)
+		return (0);
+
+	current = *h;
+
+	while (current)
+	{
+		count++;
+
+		if ((current->next) >= current)
+		{
+			*h = NULL;
+			return (count);
+		}
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+	*h = NULL;
+	return (count);
+}
