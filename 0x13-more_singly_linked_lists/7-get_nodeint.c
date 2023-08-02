a3b7a92df7208e3a3e46f800a17a7118e3495b73
kweenDev <refiloe15radebe@gmail.com>
2023-08-02 08:28:27 +0200

    Rectified function error and will continue finding nodes with the eye of a data sorcerer

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
-* get_nodeint - Returns the nth node of a listint_t list.
+* get_nodeint_at_index - Returns the nth node of a listint_t list.
 * @head: Pointer to the head of the list.
 * @index: Index of the node to be returned (starting from 0).
 *
 * Return: The nth node or NULL if it doesn't exist.
 */
-listint_t *get_nodeint(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
