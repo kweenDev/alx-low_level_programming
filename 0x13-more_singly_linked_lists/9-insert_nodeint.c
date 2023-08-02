965ec0560d3386df69096bee3bc85e6a914ff9ec
kweenDev <refiloe15radebe@gmail.com>
2023-08-02 08:56:38 +0200

    Append function to keep inserting nodes with a dash of enchantment

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,14 +1,15 @@
 #include "lists.h"
 
 /**
-* insert_nodeint - Inserts a new node at a given position in a listint_t list.
+* insert_nodeint_at_index - Inserts a new node at a given position
+* in a listint_t list.
 * @head: Pointer to a pointer to the head of the list.
 * @idx: Index of the new node to be inserted (starting from 0).
 * @n: Integer to be added as the value of the new node.
 *
 * Return: Address of the new element or NULL if it failed.
 */
-listint_t *insert_nodeint(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	listint_t *new_node, *prev_node = NULL;
