9f2fdcd070d36d6787be9885ac9a08323788ca53
kweenDev <refiloe15radebe@gmail.com>
2023-08-02 15:13:06 +0200

    Refactored code to align with Betty coding style and Safely print lists with a shield of protection

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+* print_list - Print list nodes until a specified node is reached.
+* @head: Pointer to the head of the list.
+* @end: Pointer to the end node of the list.
+*
+* Return: The number of nodes printed.
+*/
+size_t print_list(const listint_t *head, const listint_t *end)
+{
+	size_t count = 0;
+
+	while (head != end)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		count++;
+	}
+
+	printf("[%p] %d\n", (void *)head, head->n);
+	count++;
+	return (count);
+}
+
+/**
 * print_listint_safe - Prints a listint_t linked list.
 * @head: Pointer to the head of the list.
 *
@@ -8,9 +31,9 @@
 */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
 	const listint_t *slow = head, *fast = head;
 	int loop_flag = 0;
+	size_t count;
 
 	if (!head)
 		return (0);
@@ -30,26 +53,13 @@ size_t print_listint_safe(const listint_t *head)
 	if (loop_flag)
 	{
 		slow = head;
-		while (slow != fast)
-		{
-			printf("[%p] %d\n", (void *)slow, slow->n);
-			slow = slow->next;
-			count++;
-			fast = fast->next;
-		}
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		count++;
+		count = print_list(slow, fast);
 		printf("-> [%p] %d\n", (void *)fast, fast->n);
+		return (count + 1);
 	}
 	else
 	{
-		while (head)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-			count++;
-		}
+		count = print_list(head, NULL);
+		return (count);
 	}
-
-	return (count);
 }
