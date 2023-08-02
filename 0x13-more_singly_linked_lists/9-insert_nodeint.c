e308046c72c41b73ba1b6817c9eb4a31cfb6fff1
kweenDev <refiloe15radebe@gmail.com>
2023-08-02 11:01:37 +0200

    Updated 9-insert_nodeint.c: Added detailed comments explaining the function's purpose, parameters, and return value. Introduced aux_node to traverse the list for insertion.Created new_node using dynamic memory allocation. Handled border case for inserting at the beginning of the list. Implemented insertion at the specified index in the general case. Returns new node address on successful insertion, or NULL if index is out of range or memory allocation fails.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,34 +11,41 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	listint_t *new_node, *prev_node = NULL, *current_node = *head;
+	listint_t *aux_node = *head;
+	listint_t *new_node;
+	unsigned int index;
+	unsigned int cont = 0;
 
-	if (idx == 0)
-		return (add_nodeint(head, n));
-
+	/* create node */
 	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
 
-	if (new_node)
+	/* border case for insert at the beginning */
+	if (idx == 0)
 	{
-		new_node->n = n;
-		new_node->next = NULL;
-
-		while (current_node && i < idx)
-		{
-			prev_node = current_node;
-			current_node = current_node->next;
-			i++;
-		}
-
-		if (i == idx)
-		{
-			new_node->next = current_node;
-			prev_node->next = new_node;
-			return (new_node);
-		}
-
-		free(new_node);
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
 	}
+
+	/* search of position to insert */
+	index = idx - 1;
+	while (aux_node && cont != index)
+	{
+		cont++;
+		aux_node = aux_node->next;
+	}
+
+	/* general case */
+	if (cont == index && aux_node)
+	{
+		new_node->next = aux_node->next;
+		aux_node->next = new_node;
+		return (new_node);
+	}
+
+	free(new_node);
 	return (NULL);
 }
