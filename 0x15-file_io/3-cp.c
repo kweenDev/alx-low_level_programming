79a557a667226329144543ff36d819c2f7cccebb
kweenDev <refiloe15radebe@gmail.com>
2023-08-14 23:40:07 +0200

    Add program that copies content of one file to another, which handles errors better and provides informative error messages, including proper path handling in error messages. Further update: simplify code into smaller functions, to avoid too many lines in a single function. Also ensure that source file descriptor is not prematurely closes, even if an error occurs during writing to the destination file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,6 +16,65 @@ void error_exit(char *msg, int exit_code)
 }
 
 /**
+* open_files - Open source and destination files.
+* @src_filename: Source file name.
+* @dest_filename: Destination file name.
+* Return: Array containing file descriptors for source and
+* destination files.
+*/
+int *open_files(char *src_filename, char *dest_filename)
+{
+	int *fds = malloc(2 * sizeof(int));
+
+	fds[0] = open(src_filename, O_RDONLY);
+
+	if (fds[0] == -1)
+		error_exit("Error: Can't read from file", 98);
+
+	fds[1] = open(dest_filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fds[1] == -1)
+	{
+		close(fds[0]);
+		error_exit("Error: Can't write to file", 99);
+	}
+
+	return (fds);
+}
+
+/**
+* close_files - Close source and destination files.
+* @fds: Array containing file descriptors for source and
+*	destination files.
+*/
+void close_files(int *fds)
+{
+	if (close(fds[0]) == -1 || close(fds[1]) == -1)
+		error_exit("Error: Can't close file descriptor", 100);
+	free(fds);
+}
+
+/**
+* copy_content - Copy content from source to destination file.
+* @src_fd: Source file descriptor.
+* @dest_fd: Destination file descriptor.
+*/
+void copy_content(int src_fd, int dest_fd)
+{
+	ssize_t bytes_read, bytes_written;
+	char buffer[1024];
+
+	while ((bytes_read = read(src_fd, buffer, sizeof(buffer))) > 0)
+	{
+		bytes_written = write(dest_fd, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+			error_exit("Error: Can't write to file", 99);
+	}
+
+	if (bytes_read == -1)
+		error_exit("Error: Can't read from file", 98);
+}
+
+/**
 * main - Copy the content of one file to another.
 * @ac: Argument count.
 * @av: Argument vector.
@@ -23,43 +82,14 @@ void error_exit(char *msg, int exit_code)
 */
 int main(int ac, char **av)
 {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[1024];
+	int *fds;
 
 	if (ac != 3)
 		error_exit("Usage: cp file_from file_to", 97);
 
-	fd_from = open(av[1], O_RDONLY);
-
-	if (fd_from == -1)
-		error_exit("Error: Can't read from file", 98);
-
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	if (fd_to == -1)
-		error_exit("Error: Can't write to file", 99);
-
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit("Error: Can't write to file", 99);
-		}
-	}
-
-	if (bytes_read == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		error_exit("Error: Can't read from file", 98);
-	}
-
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-		error_exit("Error: Can't close file descriptor", 100);
+	fds = open_files(av[1], av[2]);
+	copy_content(fds[0], fds[1]);
+	close_files(fds);
 
 	return (0);
 }
