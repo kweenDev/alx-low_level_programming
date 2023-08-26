ef5b0087ce8707a65c1b287abc0e141a092a2220
kweenDev <refiloe15radebe@gmail.com>
2023-08-26 03:02:02 +0200

    Refactor code and add additional function to handle each part individually, copying, reading writing and handling errors.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,44 +11,72 @@
 */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	ssize_t bytesRead, bytesWritten;
-	char buffer[1024];
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (EXIT_FAILURE);
 	}
 
-	file_from = open(argv[1], O_RDONLY);
+	if (!copy_file(argv[1], argv[2]))
+	{
+		return (EXIT_FAILURE);
+	}
+
+	return (EXIT_SUCCESS);
+}
+
+/**
+* copy_file - Copies content from source file to destination file.
+* @source: The path to the source file.
+* @destination: The path to the destination file.
+*
+* Description: This function reads content from the source file and
+* copies it to the destination file. It handles various errors such as
+* file not found, permission issues, and write failures.
+* Return: 1 on success, 0 on failure.
+*/
+int copy_file(const char *source, const char *destination)
+{
+	int file_from = open(source, O_RDONLY);
+	int file_to = open_and_create(destination);
+	ssize_t bytesRead, bytesWritten;
+	char buffer[1024];
 
 	if (file_from < 0)
-		handle_error_reading(argv[1]);
-
-	file_to = open_and_create(argv[2]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+	source);
+		return (0);
+	}
 
 	if (file_to < 0)
-		handle_error_writing(file_from, argv[2]);
+	{
+		close(file_from);
+		return (0);
+	}
 
 	while ((bytesRead = read(file_from, buffer, sizeof(buffer))) > 0)
 	{
 		bytesWritten = write(file_to, buffer, bytesRead);
 
 		if (bytesWritten != bytesRead)
-		{
-			handle_error_writing(file_from, argv[2]);
-		}
+		handle_error_writing(file_from, destination);
+			return (0);
 	}
 
 	if (bytesRead < 0)
-		handle_error_reading(argv[1]);
-	if (close(file_from) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from);
-	if (close(file_to) < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to);
+	{
+		handle_error_reading(source), close(file_from), close(file_to);
+		return (0);
+	}
 
-	return (EXIT_SUCCESS);
+	if (close(file_from) < 0 || close(file_to) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		return (0);
+	}
+
+	return (1);
 }
 
 /**
@@ -72,7 +100,6 @@ int open_and_create(const char *filename)
 
 	if (fd < 0)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-
 	return (fd);
 }
 
