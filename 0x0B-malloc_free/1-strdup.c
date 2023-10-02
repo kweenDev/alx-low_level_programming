23ce230a8ab0dc3df95b49dfc251380274230fdc
kweenDev <refiloe15radebe@gmail.com>
2023-10-02 11:39:24 +0200

    Clone the beauty with _strdup function that duplicates a string.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _strdup - Duplicates a string.
+* @str: The string to duplicate.
+*
+* Return: Pointer to the duplicated string, or NULL if str is NULL,
+*	or if malloc fails.
+*/
+char *_strdup(char *str)
+{
+	char *dup;
+	int len, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	dup = (char *)malloc((len + 1) * sizeof(char));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		dup[i] = str[i];
+
+	return (dup);
+}
