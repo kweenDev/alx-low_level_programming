4fc239e4d0b059edb66101444d7a2b2b7a7db3ca
kweenDev <refiloe15radebe@gmail.com>
2023-10-20 03:16:40 +0200

    Releasing the linked list back to the memory realm!

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* free_list - This function frees a singly linked list and
+*		the memory allocated for its nodes.
+* @head: A pointer to the head of the linked list.
+*	The str member of each node is freed before freeing the
+*	node itself.
+*/
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
