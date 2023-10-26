4a0d7bd26f92a0add0584d1787bea0e8c52d81da
kweenDev <refiloe15radebe@gmail.com>
2023-10-27 00:34:05 +0200

    feat: Implement print_binary function
    Introduce the print_binary function to print the binary representation of an unsigned long int.
    The function uses recursion to extract and print each bit in reverse order.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_binary - Recursively prints the binary representation of an
+*		unsigned long integer.
+* @n: Unsigned long integer.
+*
+* Description: The function is designed to print the binary
+*		representation in reverse order, effectively giving the
+*		correct binary representation when the recursion
+*		unwinds.
+* Return: Void.
+*/
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
