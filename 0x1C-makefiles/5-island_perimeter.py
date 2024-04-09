6f6a89b5ab0e5f7e810bf71ea47d61984f5cf55a
kweenDev <refiloe15radebe@gmail.com>
2024-04-09 18:12:40 +0200

    Fix Pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@
 Module to calculate the perimeter of an island.
 """
 
+
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in grid.
@@ -39,6 +40,7 @@ def island_perimeter(grid):
                     perimeter -= 2
 
     return perimeter
+
 
 if __name__ == "__main__":
     grid = [
